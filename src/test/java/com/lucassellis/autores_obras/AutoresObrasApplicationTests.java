093cbc6e7d346923b630f8e464b4303c9ea07f53
Lucas <lucas.sellis@gmail.com>
2026-03-04 07:15:02 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/lucassellis/autores_obras/AutoresObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassellis.autores_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
